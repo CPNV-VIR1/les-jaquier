11b54a556bda02bf87e278710f13230b320ba8d7
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-06-14 12:57:07 +0200

    feat: bundle default nginx config in image (so it removes deployment steps)

--- push-to-prod.sh
+++ push-to-prod.sh
@@ -9,7 +9,6 @@ DB_TAR_FILE="kezboards-db.tar"
 API_GATEWAY_TAR_FILE="kezboards-api-gateway.tar"
 COMPOSE_FILE="compose-production.yaml"
 PRODUCTION_SCRIPT="production.sh"
-NGINX_CONFIG_FILE="ms-api-gateway/config/nginx/kezboard.conf"
 
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$0")
@@ -43,7 +42,6 @@ scp -P "$AWS_SSH_PORT" -i "$AWS_PRIVATE_KEY_PATH" "$DB_TAR_FILE" "$AWS_USER@$AWS
 scp -P "$AWS_SSH_PORT" -i "$AWS_PRIVATE_KEY_PATH" "$API_GATEWAY_TAR_FILE" "$AWS_USER@$AWS_HOST:$REMOTE_DIR/"
 scp -P "$AWS_SSH_PORT" -i "$AWS_PRIVATE_KEY_PATH" "$PRODUCTION_SCRIPT" "$AWS_USER@$AWS_HOST:$REMOTE_DIR/"
 scp -P "$AWS_SSH_PORT" -i "$AWS_PRIVATE_KEY_PATH" "$COMPOSE_PRODUCTION_FILE" "$AWS_USER@$AWS_HOST:$REMOTE_DIR/"
-scp -P "$AWS_SSH_PORT" -i "$AWS_PRIVATE_KEY_PATH" "$NGINX_CONFIG_FILE" "$AWS_USER@$AWS_HOST:$REMOTE_DIR/"
 
 # Connect to the EC2 instance and execute the production script
 echo "Connecting to $AWS_HOST and executing $PRODUCTION_SCRIPT..."
